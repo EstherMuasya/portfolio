785c813e9a18429ac0ee2f861b305f3a29dba08a
EstherMuasya <muasyaesther41@gmail.com>
2023-02-18 15:41:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let arr = ["john",23, true]
+console.log(arr[0], "\n",arr[2])
+let age = 18;
+if(age==18){
+    if(age > 18){
+    console.log("Hello voter");
+    
+
+}else{
+    console.log("Not eligible to vote")
+}
+}
+
+var i=10;
+
+if (i == 10) {
+
+if (i < 15){
+
+console.log("i is smaller than 15");
+
+// Nested - if statement
+
+// Will only be executed if statement above it is true
+
+if (i < 12){
+
+console.log("i is smaller than 12 too");
+
+} else {
+
+console.log("i is greater than 15");
+
+}
+
+}
+
+}
+
+function hey(name, age){
+    console.log("Hello there "+ name + " you are " + age + " years old.")
+}
+hey("Essie", 23)
